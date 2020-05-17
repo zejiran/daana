a3fdb5ab35c1c0fa217728d619976aaf8f4ef973
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-05-17 17:41:14 +0530

    Add files via upload

---
+++ pages/transfer_crypto.js
@@ -0,0 +1,29 @@
+const { Client, CryptoTransferTransaction } = require("@hashgraph/sdk");
+
+async function main() {
+    const operatorPrivateKey = process.env.OPERATOR_KEY;
+    const operatorAccount = process.env.OPERATOR_ID;
+
+    if (operatorPrivateKey == null || operatorAccount == null) {
+        throw new Error("environment variables OPERATOR_KEY and OPERATOR_ID must be present");
+    }
+
+    const client = new Client({
+        network: { "0.testnet.hedera.com:50211": "0.0.3" },
+        operator: {
+            account: operatorAccount,
+            privateKey: operatorPrivateKey
+        }
+    });
+
+    const receipt = await (await new CryptoTransferTransaction()
+        .addSender(operatorAccount, 1)
+        .addRecipient("0.0.3", 1)
+        .setTransactionMemo("sdk example")
+        .execute(client))
+        .getReceipt(client);
+
+    console.log(receipt);
+}
+
+main();
